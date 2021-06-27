0e5b2bcc49341f8adc940dd89c65686d766f4e9d
Nijat Mursali <nijatmursali@gmail.com>
2021-06-27 16:09:47 +0200

    fixes

--- main.py
+++ main.py
@@ -4,11 +4,13 @@ from PIL import Image
 import webbrowser
 import os
 import pathlib
-from re import search
 import nltk
-from nltk.tokenize import sent_tokenize, word_tokenize
-from nltk.corpus import stopwords
+from ibm_watson import TextToSpeechV1
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+import pygame
+from nltk.tokenize import word_tokenize
 from nltk.stem import PorterStemmer, WordNetLemmatizer
+#from re import search
 
 window = tk.Tk()
 window.title("Seminars in AI")
@@ -23,41 +25,36 @@ stemmer = PorterStemmer()
 lemmatizer = WordNetLemmatizer()
 # getting the text from the microphone and print it
 def listen():
-    # r = sr.Recognizer()
-    # with mic as source:
-    #     audio = r.listen(source)
-    # text = r.recognize_google(audio)
-    text = "I go to school"
+    r = sr.Recognizer()
+    with mic as source:
+        audio = r.listen(source)
+    text = r.recognize_google(audio)
+    #text = "I go to supermarket."
     words = word_tokenize(text)
+    words=[word.lower() for word in words if word.isalpha()]
     stemmed_words = [stemmer.stem(word) for word in words] #we get the stemmed version of the words
     lemmatized_words = [lemmatizer.lemmatize(word) for word in words]
     try:
-        finImg = Image.new('RGB', (512, 512))
+        returnedImage = Image.new('RGB', (512, 512))
         imageList = [] 
-        openedImages = [] 
+        finalImages = [] 
         currentChar = 0
         print(stemmed_words)
         for t in stemmed_words:
-            if search("what are you doing", text):
-                pass
             if pathlib.Path(f"./dataset/{t}.png").exists():
                 print(t)
                 imageList.append(f"./dataset/{t}.png")
             else:            
                 for char in t:
-                    if char  == " ":
-                        char = "space"
                     imageList.append(("./dataset/alphabet/{}.png").format(char.lower()))
                     currentChar += 1
         for x in imageList:
-            openedImages.append(Image.open(x)) 
-        finImg.save("out.gif", save_all=True, append_images=openedImages, duration=600, loop=0)
+            finalImages.append(Image.open(x)) 
+        print(imageList)
+        returnedImage.save("out.gif", save_all=True, append_images=finalImages, duration=600, loop=0)
         webbrowser.open("file:///" + os.path.realpath("out.gif")) 
-    except sr.UnknownValueError:
-        return "Google Speech Recognition could not understand audio"
-    except sr.RequestError as e:
-        return "Could not request results from Google Speech Recognition service; {0}".format(e)
-
+    except:
+        print("There was problem with understanding the audio. Try again.")
 
 photo = tk.PhotoImage(file=r"./static/images/mic.png")
 photo = photo.subsample(10, 10)
